0eae1fc17def9ce42cf04afaa547162fbedf5f96
Achraf Ben Younes <achrafbenyounes2012@gmail.com>
2019-06-29 17:18:40 +0200

    Develop the custom GUI class

--- notesManager_UI.py
+++ notesManager_UI.py
@@ -1,1 +1,12 @@
 from PySide2 import QtWidgets, QtGui, QtCore
+from ui.principalWindow import Ui_principalWindow
+
+class NoteCreation(QtWidgets.QWidget, Ui_principalWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.show()
+
+app = QtWidgets.QApplication([])
+note_creation = NoteCreation()
+app.exec_()
